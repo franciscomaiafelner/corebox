2fc28163cb11b755fbfbccacb781ae8cf750fec9
Francisco Felner <franciscomaiafelner@gmail.com>
2025-07-28 20:48:27 +0100

    not

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api' // A nossa base de URL para a API
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 // Interceptor: corre antes de cada pedido ser enviado
